edb7669f533723a5c779a24c552e181a6eb862f9
shashank shrivastava <shrivastava.shashank20@gmail.com>
2025-04-13 20:51:28 +0530

    made changes to orignal function to handle \n

--- test.py
+++ test.py
@@ -13,12 +13,14 @@ Input: “1”, Output: 1
 Input: “1,5”, Output: 6
 
 """
+import re
 
 def add(string):
-    if len(string)==0:
+    if len(string) == 0:
         return 0
     else:
-        return sum(list(map(int,string.split(','))))
+        numbers = re.split(',|\n', string)
+        return sum(map(int, numbers))
 
 print(add(''))
 print(add('1'))
@@ -33,4 +35,11 @@ print(add('1,2,3,4,5,6,7,8,9,10'))
 print(add('1,-2,-3,9,-3'))
 
 
+"""
+Scenario 3
+Allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)
 
+-for this we have to modify our original function to handle \n
+"""
+
+print(add("1\n2,3"))
