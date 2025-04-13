aaebacb4fa161a47aca1a5a6d2ad4e72755d6dad
shashank shrivastava <shrivastava.shashank20@gmail.com>
2025-04-13 21:23:00 +0530

    added the conditon for rest of scenarios

--- test.py
+++ test.py
@@ -98,4 +98,22 @@ Delimiters can be of any length with the following format: “//[delimiter]\n”
 """
 
 print(add('//[***]\n1***2***3'))
-print(add("//[!!]\n1!!-2!!3")) 
+# print(add("//[!!]\n1!!-2!!3")) 
+
+"""
+scenario 8
+Allow multiple delimiters like this: “//[delim1][delim2]\n” for example “//[*][%]\n1*2%3” should return 6.
+
+"""
+
+print(add("//[*][%]\n1*2%3"))
+print(add("//[***][%%]\n1***2%%3"))
+
+"""
+scenario 9
+make sure you can also handle multiple delimiters with length longer than one char
+
+"""
+print(add("//[abc][123]\n1abc2 1233"))
+
+
